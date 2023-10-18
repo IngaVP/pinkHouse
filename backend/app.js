bf6396a1a312caf127192029102f3132c667f359
Inga <ivpstar@gmail.com>
2023-10-17 20:45:46 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -50,10 +50,45 @@ if (!isProduction) {
 
   // backend/app.js
 const routes = require('./routes');
+app.use(routes); // Connect all the routes
+// ...
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// ...
+const { ValidationError } = require('sequelize');
 
 // ...
 
-app.use(routes); // Connect all the routes
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+//This should be the last middleware in the app.js file of your Express application.
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
 
