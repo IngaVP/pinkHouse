b7f22a4370ad486a32dbb9c45b6507632fc3c4fa
Inga <ivpstar@gmail.com>
2023-10-19 07:23:12 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -47,4 +47,15 @@ router.post(
     }
   );
 
+  // Log out
+
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;
