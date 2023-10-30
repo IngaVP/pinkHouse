b700726ed6e866b8670a65c403531e2804552c15
Inga <ivpstar@gmail.com>
2023-10-29 17:26:58 -0700

    delete a spot image

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -7,11 +7,30 @@ const { Review } = require('../../db/models');
 const { Spot } = require('../../db/models')
 const { ReviewImage } = require('../../db/models')
 const { User } = require("../../db/models")
-const { spotimage } = require("../../db/models")
+const { SpotImage } = require("../../db/models")
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+//delete a spot image
+router.delete("/:imageId", requireAuth,
+async (req,res) =>{
+    const { user } = req
+
+    let spot = await Spot.findOne({where:{ownerId: user.id}}, {include:[{model:SpotImage}]})
+
+    let spotId = spot.id
+    
+    let doomedImage = await SpotImage.findOne({where:{
+        spotId: spotId
+    }})
+
+    await doomedImage.destroy()
+
+    return res.json({
+        "message": "Successfully deleted"
+      })
+})
 
 
 module.exports = router
