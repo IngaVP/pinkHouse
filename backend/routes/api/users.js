b80811b84c72001bfffcc2b07c0ea02710e44118
Inga <ivpstar@gmail.com>
2023-12-11 10:26:20 -0800

    final changes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -14,19 +14,27 @@ const validateSignup = [
   check('email')
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage('Please provide a valid email.'),
+    .withMessage("Invalid email"),
+    //.withMessage('Please provide a valid email.'),
   check('username')
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage('Please provide a username with at least 4 characters.'),
-  check('username')
-    .not()
-    .isEmail()
-    .withMessage('Username cannot be an email.'),
-  check('password')
-    .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage('Password must be 6 characters or more.'),
+    .withMessage("Username is required"),
+  check('firstName')
+  .exists({ checkFalsy: true })
+  .withMessage("First Name is required"),
+  check('lastName')
+  .exists({ checkFalsy: true })
+  .withMessage("Last Name is required"),
+   // .withMessage('Please provide a username with at least 4 characters.'),
+  // check('username')
+  //   .not()
+  //   .isEmail()
+  //   .withMessage('Username cannot be an email.'),
+  // check('password')
+  //   .exists({ checkFalsy: true })
+  //   .isLength({ min: 6 })
+  //   .withMessage('Password must be 6 characters or more.'),
   handleValidationErrors
 ];
 
